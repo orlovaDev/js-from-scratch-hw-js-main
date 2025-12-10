d55962b541efb1a98451d431a2272ccae79c709e
orlovaDev <anastasiya.orlova1305@gmail.com>
2025-12-10 23:56:14 +0300

    lesson 2 task 1

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -13,6 +13,17 @@ const isVerifiedUser = true
 const hasSpecialPermission = true
 const hasTemporaryPass = false
 
-let isAccess
+let isAccess;
 
 // your code
+
+if ((isAdmin || isVerifiedUser) && (hasSpecialPermission || hasTemporaryPass) === true) {
+    isAccess = true;
+} else {
+    isAccess = false;
+}
+
+console.log(isAccess);
+
+
+
