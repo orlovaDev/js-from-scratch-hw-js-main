6c2ed8430173f832839d7f83230e02eab9529033
orlovaDev <anastasiya.orlova1305@gmail.com>
2026-01-08 19:12:24 +0300

    lesson-5 task-1

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -15,5 +15,12 @@ const game = {
     gold: 250,
     lumber: 100,
   },
-  addResource() {}
-}
+  addResource(resource, amount) {
+    if (this.resources[resource] === undefined) {
+      console.log("Invalid resource");
+      return;
+    } else {
+      this.resources[resource] = this.resources[resource] + amount;
+    }
+  },
+};
