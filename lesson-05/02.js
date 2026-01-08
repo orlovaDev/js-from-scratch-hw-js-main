44a7161ec86dfc6d8294b40799f8b90094f950cc
orlovaDev <anastasiya.orlova1305@gmail.com>
2026-01-08 19:53:46 +0300

    lesson-5 task-2

--- lesson-05/02.js
+++ lesson-05/02.js
@@ -30,9 +30,11 @@ console.log(gallery)
 */
 
 const gallery = {
-  'Mona Lisa': 'Leonardo da Vinci',
-  'Starry Night': 'Vincent van Gogh',
-  'The Scream': 'Edvard Munch',
-}
+  "Mona Lisa": "Leonardo da Vinci",
+  "Starry Night": "Vincent van Gogh",
+  "The Scream": "Edvard Munch",
+};
 
-function updateGallery() {}
+function updateGallery(gallery, title, newMeaning) {
+  gallery[title] = newMeaning;
+}
