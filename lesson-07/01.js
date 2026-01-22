d395c2185ca3beafdab9d40fe074807fb6f21f73
orlovaDev <anastasiya.orlova1305@gmail.com>
2026-01-22 22:37:38 +0300

    lesson7 task-1

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -13,7 +13,33 @@
 */
 
 const WEB_TECH_IMAGES = [
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
-]
+  "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg",
+  "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg",
+  "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg",
+];
+
+const imgElem = document.getElementById("web-tech-image");
+const prevButton = document.getElementById("prev-button");
+const nextButton = document.getElementById("next-button");
+
+let indexCurr = 0;
+
+let updateImg = () => {
+  imgElem.src = WEB_TECH_IMAGES[indexCurr];
+};
+
+nextButton.addEventListener("click", () => {
+  indexCurr++;
+  if (indexCurr >= WEB_TECH_IMAGES.length) {
+    indexCurr = 0;
+  }
+  updateImg();
+});
+
+prevButton.addEventListener("click", () => {
+  indexCurr--;
+  if (indexCurr < 0) {
+    indexCurr = WEB_TECH_IMAGES.length - 1;
+  }
+  updateImg();
+});
