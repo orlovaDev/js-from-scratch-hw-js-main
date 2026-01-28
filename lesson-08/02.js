e13d9e78ed8acfdb83ad3966488ffa3dc19754fd
orlovaDev <anastasiya.orlova1305@gmail.com>
2026-01-28 21:46:32 +0300

    lesson-8 task2

--- lesson-08/02.js
+++ lesson-08/02.js
@@ -4,24 +4,30 @@
  */
 
 function isNumeric(str) {
- const newStr = str.trim()
- if (newStr === "" || newStr === ".") {
-return false
- }
- let hasDot = false;
- for (let i = 0; i < newStr.length; i++) {
-   const char = newStr[i];
-   if (char >= "0" && char <= "9") {
-    continue
-   } else if (char === "." && !hasDot) {
-    hasDot = true
-   } else {
-    return false
-   }
- }
- return true
+  const newStr = str.trim();
+  if (newStr === "" || newStr === "." || newStr === "-" || newStr === "e") {
+    return false;
+  }
+  let hasDot = false;
+  let hasE = false;
+  for (let i = 0; i < newStr.length; i++) {
+    const char = newStr[i];
+    if (char >= "0" && char <= "9") {
+      continue;
+    } else if (char === "-" || char === "+") {
+      if (i !== 0 && newStr[i - 1] !== "e") return false;
+      if (i === newStr.length - 1) return false;
+    } else if (char === "." && !hasDot && !hasE) {
+      hasDot = true;
+      if (newStr.length === 1) return false;
+    } else if (char === "e" && !hasE && i !== 0 && i !== newStr.length - 1) {
+      hasE = true;
+    } else {
+      return false;
+    }
+  }
+  return true;
 }
-
 // console.log(isNumeric("123")) // Ожидаемый результат: true
 // console.log(isNumeric("12.3")) // Ожидаемый результат: true
 // console.log(isNumeric("123abc")) // Ожидаемый результат: false
