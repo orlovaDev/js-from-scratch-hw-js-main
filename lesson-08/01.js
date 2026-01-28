ecc4fae9bb869189a1bfcc7a74a96bb9bf14d665
orlovaDev <anastasiya.orlova1305@gmail.com>
2026-01-28 20:53:12 +0300

    lesson-8 task-1

--- lesson-08/01.js
+++ lesson-08/01.js
@@ -6,4 +6,14 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+function capitalizeWords(str) {
+  let newStr = "";
+  for (let i = 0; i < str.length; i++) {
+    if (i === 0 || str[i - 1] === " ") {
+      newStr += str[i].toUpperCase();
+    } else {
+      newStr += str[i];
+    }
+  }
+  return newStr;
+}
